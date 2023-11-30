18c964d79290360f332dfe3e473f7cbba464df6d
Margarita Mejia <marmejiam93@gmail.com>
2023-11-30 16:01:56 +0100

    done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,8 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
   end
+
+  def after_sign_in_path_for(resource)
+    ingredients_path
+  end
 end
